a8b0f199492b8c87e6174f352918eff6f6324614
Cristian Salas Ventura <240186@utxicotepec.edu.mx>
2026-03-13 14:35:17 -0600

    Crear Servidor

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const http = require('http');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, rest)=> rest.status(200).send({
+    message: 'Hola Crack bienvenido a mi API de Tienda Virtual',
+}));
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+module.exports = app;
